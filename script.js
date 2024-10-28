5b2fb6a27c2c8ff92c332b354f2d6d8bed94fa30
ludwig <ludwigstridh@gmail.com>
2024-10-28 11:23:12 +0100

    started

--- script.js
+++ script.js
@@ -1,13 +1,68 @@
-const word = "Word";
-const keysPressed = [];
+const thirdLetterWord = ["Cat", "Dog", "Sun", "Pen", "Hat"];
+const fourLetterWord = ["Book", "Tree", "Fish", "Ball", "Door"];
+const fiveLetterWord = ["Apple", "House", "Bread", "Water", "Light"];
+let keysPressed = [];
+let guesses = 0;
 
-let wordSplit = word.split("");
-console.log(wordSplit);
+document.addEventListener("keydown", buttonPressed);
 
-document.addEventListener("keydown", (event) => {
-	// Check if the key is not already in the array to avoid duplicates (optional)
-	if (!keysPressed.includes(event.key)) {
-		keysPressed.push(event.key);
+function startGame() {
+	wordPicker();
+}
+
+function buttonPressed(pressed) {
+	if (!keysPressed.includes(pressed.key)) {
+		keysPressed.push(pressed.key);
 	}
-	console.log(keysPressed); // Log the updated array to the console
-});
+	console.log(pressed.key);
+	winOrLose();
+
+	if (word.length == 3) {
+		if (
+			pressed.key == word[0] ||
+			pressed.key == word[1] ||
+			pressed.key == word[2]
+		) {
+		}
+	} else if (word.length == 4) {
+	} else if (word.length == 5) {
+	}
+}
+
+function wordPicker() {
+	let rnd = Math.floor(Math.random() * 3);
+	let word;
+	if (rnd == 0) {
+		word =
+			thirdLetterWord[Math.floor(Math.random() * thirdLetterWord.length)].split(
+				""
+			);
+
+		console.log(word);
+	} else if (rnd == 1) {
+		word =
+			fourLetterWord[Math.floor(Math.random() * fourLetterWord.length)].split(
+				""
+			);
+
+		console.log(word);
+	} else {
+		word =
+			fiveLetterWord[Math.floor(Math.random() * fiveLetterWord.length)].split(
+				""
+			);
+
+		console.log(word);
+		word.forEach((letter) => {
+			//show rectangles for letters in word
+		});
+	}
+}
+
+function winOrLose() {
+	//if guesses 5 lose
+	//check css hidden or innerText
+	//guess = 5 lose else win
+}
+
+startGame();
